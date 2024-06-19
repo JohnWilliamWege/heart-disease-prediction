98b227a73c3676aaa60ac4e106f1bc739e919a29
JohnWilliamWege <JohnWege16@gmail.com>
2024-06-19 19:27:48 +0200

    Heart Disease Draft

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ import streamlit as st
 from multiapp import MultiApp
 import predict
 import overview
-import report
+
 
 app = MultiApp()
 
 
 app.add_app("Predict", predict.app)
 app.add_app("Overview", overview.app)
-app.add_app("Report", report.app)
 
 # The main app
 app.run()
--- report.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def app():
-    st.title("Report")
-    st.write("The Report Will be a PDF, connected to this page if possible")
